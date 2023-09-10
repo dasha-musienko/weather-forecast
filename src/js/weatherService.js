002190a849501deb0c836f17d3012853d3688e9c
Dasha Musienko <60947524+dasha-musienko@users.noreply.github.com>
2023-09-10 22:38:24 +0300

    adds https service

--- src/js/weatherService.js
+++ src/js/weatherService.js
@@ -1,5 +1,5 @@
 export function fetchWeather(city) {
-  const BASE_URL = 'http://api.weatherapi.com/v1/';
+  const BASE_URL = 'https://api.weatherapi.com/v1/';
   const API_KEY = '6f3bf7a06a84400a8c1181835232108';
 
   return fetch(`${BASE_URL}forecast.json?key=${API_KEY}&q=${city}`).then(
